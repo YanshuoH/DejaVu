28a3d99ee47e4d01c026cc6e666935e2a815cd5a
YanshuoH <hys235606751@gmail.com>
2014-05-12 15:05:24 +0200

    add date time picker, increase accurancy

--- public/javascripts/plugins.js
+++ public/javascripts/plugins.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     /* date pickers in query form */
-    $('.input-date').datepicker();
+    $('.input-date').datetimepicker();
 
     $('#start_date').on('change', function() {
+        console.log($(this).val());
         $('#end_date').val($(this).val());
     });
     $('#query-form').submit(displayStriped);
@@ -11,7 +12,7 @@ $(document).ready(function() {
 function displayStriped(event) {
     event.preventDefault();
     var content = '';
-    content += '<div class="alert alert-info">In process, please wait a few secondes...</div>';
+    content += '<div class="alert alert-info">In process, please wait a few minutes...</div>';
     content += '<div class="progress progress-striped active">';
     content += '<div class="progress-bar"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">';
     content += '<span class="sr-only">45% Complete</span>';
