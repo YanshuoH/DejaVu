7f3748d3e7681c9f0be51b0a94c4bf916c3f0ebc
YanshuoH <hys235606751@gmail.com>
2014-05-19 15:16:00 +0200

    add index input from frontend

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@
 var async = require('async');
 var mongoose = require('mongoose');
 var QueryModel = mongoose.model('QueryModel');
+var ResultModel = mongoose.model('ResultModel');
 var TwitterSearch = require('../lib/TwitterSearch');
-
+var Kernel = require('../lib/Kernel');
 exports.index = function(req, res) {
     return res.render('index', {
         title: 'Welcome!!',
@@ -27,19 +28,43 @@ exports.index = function(req, res) {
 
 exports.run = function(req, res) {
     req.body.created_date = new Date();
-    var q = new QueryModel(req.body);
-    q.users = [];
-    q.save(function(err) {
-        if (err) {
-            var err_info = utils.errors(err);
-            req.flash('warning', err_info);
+    // TODO: check existing
+    // Wait for saving tweets,
+    // Then run Kernel_mapreduce
+    async.waterfall([
+        function(callback) {
+            var q = new QueryModel(req.body);
+            q.users = [];
+            q.save(function(err) {
+                if (err) {
+                    var err_info = utils.errors(err);
+                    req.flash('errors', err_info);
+                    return res.redirect('/');
+                }
+                else {
+                    var data = req.body;
+                    data.queryObj = q._id;
+                    // Run TwitterSearch utils, add schedule in process
+                    var ts = new TwitterSearch(data);
+                    callback(null, q);
+                }
+            });
+        },
+        // Set an hour after schedule TwitterSearch
+        function(queryObj, callback) {
+            var results = new ResultModel({query_id: queryObj._id});
+            results.save(function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            var kernel = new Kernel(queryObj, results._id);
+            setTimeout(function() {
+                kernel.buildSchedule(kernel);
+            }, 1000 * 3600);
+            callback(null, results._id);
         }
-        else {
-            var data = req.body;
-            data.queryObj = q._id;
-            // Run TwitterSearch utils, add schedule in process
-            var ts = new TwitterSearch(data);
-        }
+    ], function(err, results_id) {
+        return res.redirect('/');
     });
-    return res.redirect('/');
 }
